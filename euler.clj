e3214a8ac6e793ba26263e36a18de2abe508c892
Damon Zucconi <damonzucconi@gmail.com>
2013-06-25 00:48:09 -0300

    Update euler.clj

--- euler.clj
+++ euler.clj
@@ -11,3 +11,15 @@
 (problem-1)
 ; => 233168
 
+(defn problem-2 []
+  "By considering the terms in the Fibonacci sequence whose
+  values do not exceed four million, find the sum of the
+  even-valued terms."
+  (reduce +
+        (filter even?
+                (take-while #(> 4000000 %)
+                            (map first
+                                 (iterate (fn [[a b]] [b (+ a b)]) [0 1]))))))
+
+(problem-2)
+; => 4613732
